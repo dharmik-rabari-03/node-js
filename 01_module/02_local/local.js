a645bcfd4592b27efebf32e65e9879bffb223bb3
Dharmik Rabari <dharmik161616@gmail.com>
2026-04-28 11:43:19 +0530

    added

---
+++ 01_module/02_local/local.js
@@ -0,0 +1,16 @@
+function add(a, b) {
+  console.log("Addition : ", a + b);
+}
+
+function sub(a, b) {
+  console.log("substraction : ", a - b);
+}
+
+function multiply(a, b) {
+  console.log("multiply : ", a * b);
+}
+
+function division(a, b) {
+  console.log("division : ", a / b);
+}
+export { add, sub, multiply, division };
---
+++ 01_module/02_local/local2.js
@@ -0,0 +1,6 @@
+import {add,sub,multiply,division} from "./local.js"
+
+add(10,10)
+sub(10,5)
+multiply(2,3)
+division(10,2)
