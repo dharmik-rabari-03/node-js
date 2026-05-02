946cb6a9b7dce2cc2d9ffddcd59eebe8e26dab9b
Dharmik Rabari <dharmik161616@gmail.com>
2026-05-02 11:34:42 +0530

    added

--- 04_ejs/app.js
+++ 04_ejs/app.js
@@ -42,10 +42,45 @@ app.post("/add", (req, res) => {
   res.redirect("/");
 });
 
+app.get("/edit/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const student = studentList.find((s) => s.id === id);
+
+  if (!student) {
+    return res.send("Student not found ");
+  }
+
+  res.render("edit", { student });
+});
+
+app.post("/update/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const { name } = req.body;
+
+  const student = studentList.find((s) => s.id === id);
+
+  if (student) {
+    student.name = name;
+  }
+
+  res.redirect("/");
+});
+
+app.post("/delete/:id", (req, res) => {
+  const id = Number(req.params.id);
+  studentList = studentList.filter((s) => s.id !== id);
+
+  res.redirect("/");
+});
+
 const port = 5000;
+
 
 app.listen(port, (err) => {
   if (err) {
+
+
     console.log(err);
   }
 
