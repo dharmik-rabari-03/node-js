e9795ce37aefac26c5e117ee6496a24a2fae16c9
Dharmik Rabari <dharmik161616@gmail.com>
2026-05-28 15:19:56 +0530

    added

--- 08_Event-Management-System/middlewares/upload.js
+++ 08_Event-Management-System/middlewares/upload.js
@@ -1,71 +1,49 @@
 import multer from "multer";
 
-
-import fs from "fs"
+import fs from "fs";
 
 const storage = multer.diskStorage({
-
   destination: (req, file, cb) => {
-
-    let foldername = "uploads/"
+    let foldername = "uploads/";
 
     if (file.fieldname === "EventPoster") {
-      foldername += "EventPoster"
+      foldername += "EventPoster";
     } else if (file.fieldname === "EventBanner") {
-      foldername += "EventBanner"
+      foldername += "EventBanner";
     } else if (file.fieldname === "EventSpeaker") {
-      foldername += "EventSpeaker"
+      foldername += "EventSpeaker";
     } else if (file.fieldname === "EventDocument") {
-      foldername += "EventDocument"
+      foldername += "EventDocument";
     } else {
-      foldername += "others"
+      foldername += "others";
     }
 
+    fs.mkdirSync(foldername, { recursive: true });
 
-    fs.mkdirSync(foldername, { recursive: true })
-
-    cb(null, foldername)
-
-
-
+    cb(null, foldername);
   },
 
   filname: (req, file, cb) => {
-    const UniqueName = `${file.originalname}-${Date.now()}-${file.fieldname}`
+    const UniqueName = `${file.originalname}-${Date.now()}-${file.fieldname}`;
 
-    cb(null, UniqueName)
-
-  }
-
-})
-
-
+    cb(null, UniqueName);
+  },
+});
 
 const fileFilter = (req, file, cb) => {
-
-  const Allowed = [
-    "image/png",
-    "image/jpg",
-    "image/jpeg",
-    "application/pdf"
-  ]
+  const Allowed = ["image/png", "image/jpg", "image/jpeg", "application/pdf"];
 
   if (Allowed.includes(file.mimetype)) {
-    cb(null, true)
+    cb(null, true);
   } else {
-    cb(new Error("only jpg,jpeg andpng file are allowed",400),false)
+    cb(new Error("only jpg,jpeg and png file are allowed", 400), false);
   }
-
-
-
-}
+};
 
 const upload = multer({
-
   storage,
   fileFilter,
-  limits: { fileSize: 20 * 1024 * 1024 }
+  limits: { fileSize: 20 * 1024 * 1024 },
+});
 
-})
-
-export default upload
+export default upload;
