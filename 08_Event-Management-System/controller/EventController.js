65c4452486c1cf4f59c6ab6eafdbfdd95d04064d
Dharmik Rabari <dharmik161616@gmail.com>
2026-05-28 15:42:55 +0530

    added

--- 08_Event-Management-System/controller/EventController.js
+++ 08_Event-Management-System/controller/EventController.js
@@ -2,23 +2,16 @@ import event from "../model/eventModel.js";
 
 import httpError from "../middlewares/httpError.js";
 
-
 const create = async (req, res, next) => {
-
-
   try {
-    const {
-      EventName,
-      EventDate,
-      EventVenue,
-      EventDescription,
-      TicketPrice,
-    } = req.body;
+    const { EventName, EventDate, EventVenue, EventDescription, TicketPrice } =
+      req.body;
 
-    const EventPoster = req.files?.EventPoster?.[0]?.path || null
-    const EventBanner = req.files?.EventBanner?.map((file) => file.path) || []
-    const EventSpeaker = req.files?.EventSpeaker?.map((file) => file.path) || []
-    const EventDocument = req.files?.EventDocument?.[0]?.path || null
+    const EventPoster = req.files?.EventPoster?.[0]?.path || null;
+    const EventBanner = req.files?.EventBanner?.map((file) => file.path) || [];
+    const EventSpeaker =
+      req.files?.EventSpeaker?.map((file) => file.path) || [];
+    const EventDocument = req.files?.EventDocument?.[0]?.path || null;
 
     const NewEvent = new event({
       EventName,
@@ -29,20 +22,17 @@ const create = async (req, res, next) => {
       EventPoster,
       EventBanner,
       EventSpeaker,
-      EventDocument
+      EventDocument,
     });
 
+    await NewEvent.save();
 
-    await NewEvent.save()
-
-    res.status(201).json({ success: true, message: "new event added", data: NewEvent })
-
+    res
+      .status(201)
+      .json({ success: true, message: "new event added", data: NewEvent });
   } catch (error) {
-
-    next(new httpError(error.message, 500))
-
+    next(new httpError(error.message, 500));
   }
-
 };
 
-export default { create }
+export default { create };
