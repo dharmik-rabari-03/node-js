1c701fde5fdef8295797a83c3c2f49f7dabfdf51
dharmik-rabari-03 <dharmik161616@gmail.com>
2026-06-27 19:24:37 +0530

    delete,update,logout and logout all device successfully

--- practice/JWT-practice/controller/userController.js
+++ practice/JWT-practice/controller/userController.js
@@ -8,7 +8,7 @@ const Add = async function (req, res, next) {
         const { name, Email, password } = req.body
 
         if (!name || !Email || !password) {
-            return new httpError("all Field Are required")
+            throw new httpError("all Field Are required")
         }
 
         const newuser = new Model({
@@ -37,13 +37,13 @@ const getAllUser = async function (req, res, next) {
         const find = await Model.find()
 
         if (find.length === 0) {
-            return new httpError("no user found")
+            throw new httpError("no user found")
         }
 
         res.status(200).json({ message: "user data found", total: find.length, date: find })
 
     } catch (error) {
-        next(new httpError(error.message))
+        throw next(new httpError(error.message))
     }
 
 }
@@ -61,12 +61,12 @@ const login = async function (req, res, next) {
         res.status(200).json({ success: true, message: "login succesfully", user })
 
     } catch (error) {
-        next(new httpError(error.message))
+        throw next(new httpError(error.message))
     }
 
 }
 
-const authLogin = async function (req,res,next) {
+const authLogin = async function (req, res, next) {
 
     try {
 
@@ -79,8 +79,90 @@ const authLogin = async function (req,res,next) {
         res.status(200).json({ success: true, message: "auth login succesfull", user })
 
     } catch (error) {
-        next(new httpError(error.message))
+        throw next(new httpError(error.message))
 
+    }
+
+}
+
+const Delete = async function (req, res, next) {
+
+    try {
+
+
+        await req.user.deleteOne()
+
+        res.status(200).json({ success: true, message: "user deleted succesfullly" })
+
+    } catch (error) {
+        next(new httpError(error.message))
+    }
+
+}
+
+const update = async function name(req, res, next) {
+
+    try {
+
+        const update = Object.keys(req.body)
+
+        const allowedField = ["name", "password"]
+
+        const isvalid = update.every((update) => {
+            return allowedField.includes(update)
+        })
+
+        if (!isvalid) {
+            throw new httpError("only allowed field can be update")
+        }
+
+        update.forEach((update) => {
+            req.user[update] = req.body[update]
+        })
+
+        await req.user.save()
+
+        res.status(200).json({ success: true, message: "user updated successfully", user: req.user })
+
+
+
+    } catch (error) {
+        throw next(new httpError(error.message))
+    }
+
+}
+
+const logout = async function (req, res, next) {
+
+    try {
+
+        req.user.tokens = req.user.tokens.filter((item) => {
+            return item.token !== req.token
+        })
+
+        res.status(200).json({ success: true, message: "logout succesfullly" })
+
+        await req.user.save()
+
+
+    } catch (error) {
+        throw new httpError(error.message)
+    }
+
+}
+
+const logoutAll = async function name(req, res, next) {
+
+    try {
+
+        req.user.tokens = []
+
+        await req.user.save()
+
+        res.status(200).json({ success: true, message: "logout from all device successfull" })
+
+    } catch (error) {
+        throw new httpError(error.message)
     }
 
 }
@@ -88,4 +170,4 @@ const authLogin = async function (req,res,next) {
 
 
 
-export default { Add, getAllUser, login ,authLogin}
+export default { Add, getAllUser, login, authLogin, Delete, logout, logoutAll ,update}
--- practice/JWT-practice/routes/userRoute.js
+++ practice/JWT-practice/routes/userRoute.js
@@ -7,5 +7,9 @@ router.post("/add", UserController.Add)
 router.get("/GetAllUser", UserController.getAllUser)
 router.post("/login", UserController.login)
 router.get("/authlogin", auth, UserController.authLogin)
+router.delete("/delete", auth, UserController.Delete)
+router.patch("/update",auth,UserController.update)
+router.post("/logout",auth,UserController.logout)
+router.post("/logoutAll",auth,UserController.logoutAll)
 
 export default router
