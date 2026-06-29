08cc0a42f797e4d3752571a27cbc0ce554b7f2ff
Dharmik Ragiya <dharmik161616@gmail.com>
2026-06-29 14:50:24 +0530

    Update userRoute.js

--- 11_JWT_Authentication/routes/userRoute.js
+++ 11_JWT_Authentication/routes/userRoute.js
@@ -7,10 +7,18 @@ const router = express.Router();
 
 router.post("/add", userController.Add);
 
-router.get("/GetAllUser", auth,userController.GetAllUser);
+router.get("/GetAllUser", auth, userController.GetAllUser);
 
 router.post("/login", userController.loggin);
 
 router.get("/authLogin", auth, userController.AuthLoggin);
+
+router.get("/logout", auth, userController.logout);
+
+router.get("/logutAll", auth, userController.logutAll);
+
+router.patch("/Update",auth,userController.UpdateUser)
+
+router.delete("/DeleteUser",auth,userController.DeleteUser)
 
 export default router;
